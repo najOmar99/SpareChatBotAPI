4f2e0e93267d1de62cf5091ffac2fdd407c2054e
najOmar99 <71253358+najOmar99@users.noreply.github.com>
2021-05-24 11:01:00 +0300

    latest changes , updated

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request
 from chatterbot import ChatBot
 from chatterbot.trainers import ChatterBotCorpusTrainer
+from datetime import date
 
 app = Flask(__name__)
 app.debug = True
@@ -12,13 +13,15 @@ trainer.train("chatterbot.corpus.english")
 
 @app.route("/")
 def home():
-    return render_template("message.html")
+    return render_template("message.html", datetime=str(date.today()))
 
 
-@app.route("/get")
+@app.route("/get_bot_response", methods=["GET", "POST"])
 def get_bot_response():
     userText = request.args.get('msg')
     return str(english_bot.get_response(userText))
+    # return redirect ("message.html")
+
 
 
 if __name__ == '__main__':
