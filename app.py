bc0c0543434c09de053b085cb52c3a8332e0c5c8
najOmar99 <71253358+najOmar99@users.noreply.github.com>
2021-05-25 10:03:09 +0300

    cross scripting error handled , response showing

--- app.py
+++ app.py
@@ -1,9 +1,14 @@
-from flask import Flask, render_template, request
+import json
+
+from flask import Flask, render_template, request, redirect
 from chatterbot import ChatBot
 from chatterbot.trainers import ChatterBotCorpusTrainer
 from datetime import date
+from flask import jsonify
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 app.debug = True
 english_bot = ChatBot("Chatterbot", storage_adapter="chatterbot.storage.SQLStorageAdapter")
 trainer = ChatterBotCorpusTrainer(english_bot)
@@ -18,10 +23,23 @@ def home():
 
 @app.route("/get_bot_response", methods=["GET", "POST"])
 def get_bot_response():
-    userText = request.args.get('msg')
-    return str(english_bot.get_response(userText))
+    # userText = request.POST('msg')
+    userText = request.form['msg']
+    print(request.form['msg'])
+    # userText = request.form['msg']
+    data = english_bot.get_response(userText)
+    print(data)
+    data = str(english_bot.get_response(userText))
+    # return str(english_bot.get_response(userText))
+    json_data = jsonify(data)
+    return json_data
+    # return json.dumps([{'success':True, 'data': data}]), 200, {'ContentType':'application/json'}
     # return redirect ("message.html")
 
+
+@app.route("/sendBot_answer")
+def sendBot_answer(json_data):
+    return redirect('http://localhost:8090', code=301)
 
 
 if __name__ == '__main__':
